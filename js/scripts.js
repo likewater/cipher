57d389ad1ad2e46341262905aa3459abd9d1c101
Blake Womack and Dallas Slaughter <dallas@>
2017-02-13 14:58:12 -0800

    Add several functions to concacate certain parts of the string, reverse string, lots of commented out bits.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,44 @@
 var sentence = prompt("Enter a sentence: ");
-/*var lastLetter = /^./.match(sentence);*/
 var lastLetter = sentence.charAt(sentence.length-1);
 var firstLetter = sentence.charAt(0);
 
-var firstLast = function(sentence) {
-  return lastLetter;
+var firstLast = function() {
+  return (firstLetter + lastLetter).toUpperCase();
 }
 
+
+
+var lastFirst = function() {
+  var firstShort = firstLast().charAt(0);
+  var lastShort = firstLast().charAt(1);
+  return (lastShort + firstShort).toUpperCase();
+}
+
+var thirdFunction = function() {
+  firstLast();
+  return sentence + lastFirst();
+}
+
+var fourthFunction = function() {
+  var senLength = parseInt(sentence.length);
+  var senHalf = senLength / 2;
+  var nthLetter = sentence.charAt(senHalf);
+  return nthLetter + thirdFunction();
+}
+
+var lastFunction = function() {
+  return fourthFunction().split("").reverse().join("");
+}
+/*
+alert(fourthFunction());
+alert(firstLast());
+alert(lastFirst());
+alert(thirdFunction());
+*/
+alert(fourthFunction());
+alert(lastFunction());
+
+/*
 alert(lastLetter.toUpperCase());
 alert(firstLetter.toUpperCase());
 /*
