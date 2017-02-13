9af6ea4cac540d72cc87142e310e759f0b32c391
Blake Womack and Dallas Slaughter <dallas@>
2017-02-13 13:56:04 -0800

    added first regex expressions that return first and last letters.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var sentence = prompt("Enter a sentence: ");
+/*var lastLetter = /^./.match(sentence);*/
+var lastLetter = sentence.charAt(sentence.length-1);
+var firstLetter = sentence.charAt(0);
+
+var firstLast = function(sentence) {
+  return lastLetter;
+}
+
+alert(lastLetter.toUpperCase());
+alert(firstLetter.toUpperCase());
+/*
+var firstLastRev
+
+var sentenceToRev
+
+var confuseSentence
+*/
