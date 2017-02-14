0d24b79548069ff9783e911b5b069fca1387cfd3
Blake Womack and Dallas Slaughter <blake@>
2017-02-13 16:07:24 -0800

    Add h2 and h3 tags to show sentence before and after cipher when clicked on

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,32 @@
-var sentence = prompt("Enter a sentence: ");
-var lastLetter = sentence.charAt(sentence.length-1);
-var firstLetter = sentence.charAt(0);
+var sentence = prompt("Enter a sentence: "); //take some input
+var lastLetter = sentence.charAt(sentence.length-1); //find letter at end
+var firstLetter = sentence.charAt(0); //find first letter
 
 var firstLast = function() {
-  return (firstLetter + lastLetter).toUpperCase();
+  return (firstLetter + lastLetter).toUpperCase(); //concat first and last letter together and then uppercase it
 }
-
-
-
-var lastFirst = function() {
+var lastFirst = function() { //take shorted letters and reverse
   var firstShort = firstLast().charAt(0);
   var lastShort = firstLast().charAt(1);
   return (lastShort + firstShort).toUpperCase();
 }
-
 var thirdFunction = function() {
   firstLast();
   return sentence + lastFirst();
 }
-
 var fourthFunction = function() {
   var senLength = parseInt(sentence.length);
   var senHalf = senLength / 2;
   var nthLetter = sentence.charAt(senHalf);
   return nthLetter + thirdFunction();
 }
-
 var lastFunction = function() {
   return fourthFunction().split("").reverse().join("");
 }
-/*
-alert(fourthFunction());
-alert(firstLast());
-alert(lastFirst());
-alert(thirdFunction());
-*/
-alert(fourthFunction());
-alert(lastFunction());
 
-/*
-alert(lastLetter.toUpperCase());
-alert(firstLetter.toUpperCase());
-/*
-var firstLastRev
-
-var sentenceToRev
-
-var confuseSentence
-*/
+$("h2").click(function() {
+  alert(sentence);
+});
+$("h3").click(function() {
+  alert(lastFunction());
+});
